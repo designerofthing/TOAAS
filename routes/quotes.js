406916f0399133a140d51e7f799ab86618ea5e5e
designerofthing <designerofthings@gmail.com>
2020-06-17 21:33:54 +0200

    adds randomiser function

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const quotesCollection = require('../quotes.json');
+quote = () => quotesCollection[Math.random() * quotesCollection.length>>0]
 
 /* GET quotes listing. */
 router.get('/', (req, res) => {
-  res.json({quotes: quotesCollection});
+  res.json({quotes: quote()});
 });
 
 module.exports = router;
