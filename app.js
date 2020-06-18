fee3210f232ce368e31fb71a6c0126bd9f8c96b3
designerofthing <designerofthings@gmail.com>
2020-06-18 10:36:13 +0200

    adds more quotes, installs jade to show index info

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var quotes = require('./routes/quotes');
+var index = require('./routes/index');
 
 var app = express();
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
+app.use('/', index);
 app.use('/quotes', quotes);
 
 
